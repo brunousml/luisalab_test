c8432572dc9d66945264bbcbe92c5b8855ea4cfe
Bruno Melo <brunousml@gmail.com>
2017-06-18 00:38:58 -0300

    change email field to correct type

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     department = models.CharField(max_length=255)
 
     def __str__(self):
